337162a945ac0329bf6af8eae971eecffccbc57e
Alessandro Verlato <averlato@gmail.com>
2012-11-17 21:35:17 +0100

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodemotionHackaton::Application.config.secret_token = '6d1ccf07d8e1e3af7b77f9d8ca034e626e172886a5c7975771cf0138c64b5468c9e921e31e9754e54542b02a44dd26ba79c2d24bc4b9decd5c374ac3d083a86e'
