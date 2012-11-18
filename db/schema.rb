c8270e3214779eb28b4dde1a32c3c945bd3f14df
Luca Ferrari <luca.ferrari@risingbits.com>
2012-11-18 03:44:24 +0100

    Schema change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121118014435) do
+ActiveRecord::Schema.define(:version => 20121118015734) do
 
   create_table "need_types", :force => true do |t|
     t.string   "name"
