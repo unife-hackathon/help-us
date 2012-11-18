122837fad1877bb0242f3a1c1c1e84ab72babb8e
Marco Bersani <marco.bersani@risingbits.com>
2012-11-18 04:34:22 +0100

    add marker picture in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,8 @@ s_ago_school = Organization.create(:name => "Nuova scuola di S.Agostino",
                     :description => "Costruiamo insieme la nuova scuola a S.Agostino!",
                     :logo => "http://img.funtasticus.com/2008/nov/111512Amateurs/amateur%20girls%2015.jpg",
                     :latitude => "44.7938",
-                    :longitude => "11.3813")
+                    :longitude => "11.3813",
+                    :marker_path => "../assets/icons/school.png")
 
 mensa_finale = Organization.create(:name => "Mensa di Finale Emilia",
                     :description => "Aiutiamo le persone a superare questo momento difficile!",
