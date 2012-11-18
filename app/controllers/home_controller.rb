0ad56e6cbb1cd65c43e9c64c45b135690c06e0a9
Michele Tedeschi <michele.tedeschi@gmail.com>
2012-11-18 09:20:07 +0100

    Merge home controller mods

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
 
   def index
-    #@markers = markers
     @needs = Need.includes(:organization).order('created_at desc').last(5)
     respond_to do |format|
       format.html { @markers = markers('html').to_json}
@@ -12,15 +11,15 @@ class HomeController < ApplicationController
 
   private
 
-  def markers
-    Organization.all.to_gmaps4rails do |org, marker|
-      marker.infowindow render_to_string(:partial => "organizations/organization", :locals => { :organization => org})
-      marker.picture({
-                      :picture => org.marker_path,
-                      :width   => 48,
-                      :height  => 48
-                     })
+  def markers(format)
+    markers = []
+    Organization.all.each do |org|
+      @needs = org.needs
+      markers << { :lat => org.latitude, :lng => org.longitude,
+                   :picture => org.marker_path, :width => 48, :height => 48 }
+      markers.last[:description] = render_to_string(:partial => "organizations/organization", :locals => { :organization => org}) if format.eql? 'html'
     end
+    markers
   end
 
 end
