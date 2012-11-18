316475d1148301b680ad2566f2460064e4223bba
Marco Bersani <marco.bersani@risingbits.com>
2012-11-18 02:15:23 +0100

    add latitude and longitude data to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
 Organization.create(:name => "Nuova scuola di S.Agostino",
                     :description => "Costruiamo insieme la nuova scuola a S.Agostino!",
-                    :logo => "http://img.funtasticus.com/2008/nov/111512Amateurs/amateur%20girls%2015.jpg")
+                    :logo => "http://img.funtasticus.com/2008/nov/111512Amateurs/amateur%20girls%2015.jpg",
+                    :latitude => "44.7938",
+                    :longitude => "11.3813")
 Organization.create(:name => "Mensa di Finale Emilia",
                     :description => "Aiutiamo le persone a superare questo momento difficile!",
-                    :logo => "http://img.funtasticus.com/2008/nov/111512Amateurs/amateur%20girls%208.jpg")
+                    :logo => "http://img.funtasticus.com/2008/nov/111512Amateurs/amateur%20girls%208.jpg",
+                    :latitude => "44.8311",
+                    :longitude => "11.2919")
 
