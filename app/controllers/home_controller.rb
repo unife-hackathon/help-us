1fcd4564215d985d88cc44faf8f82557e2d71653
Alessandro Verlato <averlato@gmail.com>
2012-11-18 08:48:29 +0100

    Markers JSON

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,22 +1,26 @@
 class HomeController < ApplicationController
 
   def index
-    @markers = markers
+    #@markers = markers
     @needs = Need.includes(:organization).order('created_at desc').last(5)
+    respond_to do |format|
+      format.html { @markers = markers('html').to_json}
+      format.json { render json: markers('json').to_json   }
+    end
   end
 
 
   private
 
-  def markers
+  def markers(format)
     markers = []
     Organization.all.each do |org|
       @needs = org.needs
-      markers << {:lat => org.latitude, :lng => org.longitude,
-                  :picture => org.marker_path, :width => 48, :height => 48,
-                  :description => render_to_string("popup_content", :layout => false)}
+      markers << { :lat => org.latitude, :lng => org.longitude,
+                   :picture => org.marker_path, :width => 48, :height => 48 }
+      markers.last[:description] = render_to_string("popup_content", :layout => false) if format.eql? "html"
     end
-    markers.to_json
+    markers
   end
 
 end
