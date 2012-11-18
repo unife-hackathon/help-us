4026093f1cf9c7df5239aa4378ab3d2f9ec04be7
Giulio Grillanda <giulio.grillanda@gmail.com>
2012-11-18 11:26:15 +0100

    setup internationalization, devise included

--- config/application.rb
+++ config/application.rb
@@ -29,9 +29,9 @@ module CodemotionHackaton
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    # The default locale is :it and all translations from config/locales/*.rb,yml are auto loaded.
+     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+     config.i18n.default_locale = :it
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
