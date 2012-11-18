3111fc9c3fc0c8abde79ed7097432091d07d4b2b
Luca Ferrari <luca.ferrari@risingbits.com>
2012-11-18 12:14:56 +0100

    Remove NeedsController#index action

--- app/controllers/needs_controller.rb
+++ app/controllers/needs_controller.rb
@@ -1,10 +1,4 @@
 class NeedsController < ApplicationController
-
-  def index
-    @organization = Organization.find(params[:organization_id])
-    @needs = @organization.needs
-  end
-
   def show
     @need = Need.find(params[:id])
   end
