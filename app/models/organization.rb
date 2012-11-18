073690e107d8a4e3288650c9a6a4fa333b5e120f
Alessandro Verlato <averlato@gmail.com>
2012-11-18 08:04:41 +0100

    Some funny stuff. Added popup content. Needs MOAR graphics

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -12,12 +12,4 @@ class Organization < ActiveRecord::Base
   end
 
 
-  def self.markers
-    markers = []
-    Organization.all.each do |org|
-      markers << {:lat => org.latitude, :lng => org.longitude, :picture => org.marker_path, :width => 48, :height => 48, :description => "asdasd"}
-    end
-    markers.to_json
-  end
-
 end
